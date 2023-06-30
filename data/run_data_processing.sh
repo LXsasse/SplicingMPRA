6967025948759a2f22900dc7658a8f471e55f6f6
LXsasse <alexander.sasse@gmail.com>
2023-06-30 12:15:37 -0700

    Create run_data_processing.sh

---
+++ data/run_data_processing.sh
@@ -0,0 +1,7 @@
+# Generate data and traget values for linear models
+python get_linear_preddata.py
+# Generate one-hot encodings of the random sequence elements and their target values for the CNN models
+python get_onehotencoding.py
+# To generate sequence-structure representations of the MPRA sequences follow steps in 
+./processRNA.sh
+
